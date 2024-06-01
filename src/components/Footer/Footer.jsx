affbab2fcdbfa6f7a7cfb996cf8b5adc72857752
rjtprasad <prasadrajat28@gmail.com>
2024-06-01 21:32:47 +0530

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import "./footer.scss"
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Men</span>
+          <span>Women</span>
+          <span>Children</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae totam mollitia dolorum inventore vitae? Dolor perferendis quis nostrum reiciendis est.</span>
+        </div>
+        <div className="item">
+          <h1>Contact Us</h1>
+          <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat perspiciatis magni omnis! Dignissimos nesciunt ex esse eaque maiores nisi mollitia.</span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">Royal Fashion</span>
+          <span className="copyright">Copyright 2024. All Rights Reserved</span>
+        </div>
+        <div className="right">
+          <img src="/img/payment.png" alt="" />
+        </div>
+        
+      </div>
+    </div>
+
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './navbar.scss'
 
 export const Navbar = () => {
